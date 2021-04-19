6fb5a5218347b3b3c4da50bf1f3b9c09824f94bc
TejaSai <tejasaiinala@gmail.com>
2021-04-19 11:16:03 -0400

    Initial Changes

---
+++ eureka-service/src/test/java/com/CinemaProject/Eureka/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CinemaProject.Eureka.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
